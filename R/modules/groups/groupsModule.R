f6996b4f7da5caa4935a440692149d28faaedb57
LaurenceOMFoisy <mail@mfoisy.com>
2024-12-02 16:58:51 -0500

    rm stuff

--- R/modules/groups/groupsModule.R
+++ R/modules/groups/groupsModule.R
@@ -26,7 +26,6 @@ socialGroupsUI <- function(id) {
   )
 }
 
-
 socialGroupsServer <- function(id, data) {
   moduleServer(id, function(input, output, session) {
     
@@ -53,73 +52,7 @@ socialGroupsServer <- function(id, data) {
       "Housing Status" = "ses_owner",
       "Religious Groups" = "ses_religion",
       "Sexual Orientation" = "ses_orientation_factor"
-    )
-    
-    create_plot <- function(social_var, lifestyle_var, plot_type) {
-      plot_data <- data %>%
-        select(all_of(c(social_var, lifestyle_var))) %>%
-        drop_na()
-      
-      p <- ggplot(plot_data) +
-        theme_minimal() +
-        theme(axis.text.x = element_text(angle = 45, hjust = 1))
-      
-      switch(plot_type,
-        "bar" = {
-          p + 
-            geom_bar(aes(x = .data[[social_var]], 
-                        fill = .data[[lifestyle_var]]), 
-                    position = "fill") +
-            labs(y = "Proportion", 
-                 x = names(which(choices == social_var)),
-                 title = names(which(lifestyle_vars == lifestyle_var)))
-        },
-        "box" = {
-          p +
-            geom_boxplot(aes(x = .data[[social_var]], 
-                            y = .data[[lifestyle_var]])) +
-            coord_flip() +
-            labs(title = names(which(lifestyle_vars == lifestyle_var)))
-        },
-        "violin" = {
-          p +
-            geom_violin(aes(x = .data[[social_var]], 
-                           y = .data[[lifestyle_var]])) +
-            coord_flip() +
-            labs(title = names(which(lifestyle_vars == lifestyle_var)))
-        },
-        "histogram" = {
-          p +
-            geom_histogram(aes(x = .data[[lifestyle_var]])) +
-            facet_wrap(vars(.data[[social_var]]), scales = "free_y") +
-            labs(title = names(which(lifestyle_vars == lifestyle_var)))
-        }
-      )
-    }
-    
-    output$plot_container <- renderUI({
-      plot_output_list <- lapply(seq_along(lifestyle_vars), function(i) {
-        plotname <- paste0("plot", i)
-        plotOutput(session$ns(plotname), height = "400px")
-      })
-      
-      do.call(tagList, plot_output_list)
-    })
-    
-    observe({
-      for (i in seq_along(lifestyle_vars)) {
-        local({
-          local_i <- i
-          plotname <- paste0("plot", local_i)
-          
-          output[[plotname]] <- renderPlot({
-            create_plot(input$social_var, 
-                       lifestyle_vars[[local_i]], 
-                       input$plot_type)
-          })
-        })
-      }
-    })
+    ) 
   })
 }
 
